15842876cac5c4f37c7b7345767b67ee3e3d9018
Abdulrhman-kh546 <abood.khmour@gmail.com>
2025-04-20 23:09:11 +0300

    update

--- main.dart
+++ main.dart
@@ -1,8 +1,18 @@
 
 
 void main() {
-  String myName= "Abdulrhman";
+  String myName= "abdulrhman";
+  int age = 24;
+  double myHight = 178.7;
 
-  print(myName);
+  print(myName[0].toUpperCase());
+  print(myHight.ceil());
+  print(myHight.floor());
+  print(myHight.round());
+  print(myHight.isNegative);
+
+  int myAgeAfter10Year=age+10;
+  print("my age after 10 year is: $myAgeAfter10Year");
+
   
 }
